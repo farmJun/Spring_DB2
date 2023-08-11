051038b016a2fe0f3396d16c7c40ac4f81684848
박준영 <wnsdud6969@naver.com>
2023-08-11 20:46:23 +0900

    test: 데이터베이스 연결 해제 후 테스트

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -30,7 +30,7 @@ public class ItemServiceApplication {
         return new TestDataInit(itemRepository);
     }
 
-    @Bean
+ /*   @Bean
     @Profile("test")
     public DataSource dataSource() {
         log.info("메모리 데이터베이스 초기화");
@@ -40,5 +40,5 @@ public class ItemServiceApplication {
         dataSource.setUsername("sa");
         dataSource.setPassword("");
         return dataSource;
-    }
+    }*/
 }
